c0fb7609eba37b7bbda53f4ba2219755567a3f30
Xiaoyi Liu <lxy878@gmail.com>
2020-12-01 16:05:34 +0000

    Automatically backed up by Learn

--- src/CatList.js
+++ src/CatList.js
@@ -1,8 +1,1 @@
-import React from 'react'
-
-export default function CatList({catPics}){
-    return (<ul>
-        {catPics.map(catPic=> <img key={catPic.id} src={catPic.url} alt={catPic.id}></img>)}
-    </ul>)
-}
-z
+// write your CatList component here
